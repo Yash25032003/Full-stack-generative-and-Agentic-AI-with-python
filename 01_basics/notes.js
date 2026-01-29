ece9511e2e8000b380bcef8d6d8e5b2c691b4713
Yash25032003 <yashsharma210300@acropolis.in>
2026-01-29 20:31:43 +0530

    add intro to python

---
+++ 01_basics/notes.js
@@ -0,0 +1,23 @@
+//! Q1 . What is modules in python?
+// Python me module ek file hoti hai jisme functions, variables aur classes likhe hote hain, jinko hum dusre programs me reuse kar sakte hain.
+//? example :
+//todo:  Create a file called mymodule.py
+// def add(a, b):
+// return a + b
+
+//todo:  Now use it in another file:
+// import mymodule
+// print(mymodule.add(2, 3))
+
+//! Q2. What is packages in python?
+// Python me package ek folder hota hai jisme multiple Python files (modules) hote hain aur ek special file hoti hai __init__.py.
+// Ye large projects ko proper structure dene me help karta hai.
+
+//? Folder Structure:
+// myproject/
+//  └── mathutils/
+//      ├── __init__.py
+//      ├── add.py
+//      └── subtract.py
+
+//* mathutils folder ko hai vo package hoga because vo special file contain kar raha hai and multiple python files(modules) bhi contain kar raha hai.
