225f676397b7275bd41ae620b5ff0b9ed299bbe6
Yash25032003 <yashsharma210300@acropolis.in>
2026-05-23 12:48:43 +0530

    commit came back

---
+++ 02_data_types_in_python/chapter1.py
@@ -0,0 +1,11 @@
+sugar_amount = 2 
+print(f"The initial sugar is : {sugar_amount}")
+
+sugar_amount = 12
+print(f"The second sugar is : {sugar_amount}")
+# above example me hum reference change kar rahe hai na ki value because numbers are immutable
+# proof ? by checking the id
+
+print(f"Id of 2 is : {id(2)}")
+print(f"id of 12 is : {id(12)}")
+# both id are different not same matlab they are immutable
---
+++ 02_data_types_in_python/notes.js
@@ -0,0 +1,14 @@
+//! Q1. What is object in python
+// Python me object kisi class ka instance hota hai i jisme data aur methods/actions hote hain.
+// Ye ek real-world entity hoti hai jisme:
+// 1. Data hota hai (attributes)
+// 2. Actions hote hain (methods)
+//? Simple words me Object = Data + Us data par kaam karne wale functions
+
+//* Python me almost har cheez ek object hoti hai – number, string, list, function, sab kuch.
+
+//! Mutable ?=> things that is changeable
+//? immutale ?=> things that are not changeable
+
+// object contains identity , type , value
+//todo: Kisi bhi object ki Mutable ya immutale pata karna ho to vo hum identity se hi pata kar sakte hai object ki.
